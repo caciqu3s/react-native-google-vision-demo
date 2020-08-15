1689612d32431b2b1b297046885ca44dd66134e6
caciqu3s <richardmarques016@gmail.com>
2020-08-15 13:19:52 -0300

    feat: camera and google vision api implemented

--- App.tsx
+++ App.tsx
@@ -1,21 +1,71 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Text, View, Button, ActivityIndicator } from 'react-native';
+import { Camera } from 'expo-camera';
+import axios from 'axios';
+import google_api_key from './keys/google_api_key';
 
 export default function App() {
+  const [hasPermission, setHasPermission] = useState<boolean>(false);
+  const type = Camera.Constants.Type.back;
+  const [loading, setLoading] = useState<boolean>(false);
+  const [text, setText] = useState<string>('');
+  let camera: Camera | null;
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  const takePicture = async () => {
+    if(camera) {
+      setLoading(true);
+      let photo = await camera.takePictureAsync({ base64: true });
+      axios.post(`https://vision.googleapis.com/v1/images:annotate?key=${google_api_key}`, { 
+        requests: [
+          {
+            image: {
+              content: photo.base64
+            },
+            features: [
+              {
+                type: "TEXT_DETECTION"
+              }
+            ]
+          }
+        ] 
+      })
+      .then(res => setText(res.data.responses[0].textAnnotations[0].description))
+      .finally(() => setLoading(false));
+    }
+  }
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Camera 
+        type={type}
+        ref={ref => {
+          camera = ref;
+        }}
+      >
+        <View
+          style={{
+            width: 300,
+            height: 300,
+            backgroundColor: 'transparent',
+            flexDirection: 'row',
+          }}>
+        </View>
+      </Camera>
+      <Button title="Take Picture" onPress={() => takePicture()} disabled={loading}></Button>
+      <Text>{text}</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ keys/google_api_key.ts
@@ -0,0 +1,1 @@
+export default 'YOUR GOOGLE API KEY';
